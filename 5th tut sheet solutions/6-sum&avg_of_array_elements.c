a542be9c910797a78f38579e905f9591fc64a919
IshikaW1 <100034839+IshikaW1@users.noreply.github.com>
2024-10-21 00:23:24 +0530

    Update 6-sum&avg_of_array_elements.c

--- 5th tut sheet solutions/6-sum&avg_of_array_elements.c
+++ 5th tut sheet solutions/6-sum&avg_of_array_elements.c
@@ -1,1 +1,12 @@
-
+#include <stdio.h>
+int main(){
+  int arr[6]={1,2,45,65,89,20};
+  int size=6;
+  float sum, avg;
+  for (int i=0; i<size; i++){
+    sum+=arr[i];
+  }
+  printf("Sum = %.2f", sum);
+  printf("Avg = %.2f", sum/size);
+  return 0;
+}
